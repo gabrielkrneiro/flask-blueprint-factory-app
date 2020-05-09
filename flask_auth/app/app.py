5577245f75b45b58bee5f844a28b6fd61703282f
Gabriel Carneiro <carneiro.development@gmail.com>
2020-05-08 21:15:24 -0400

    feature: Add Swagger extension

--- flask_auth/app/app.py
+++ flask_auth/app/app.py
@@ -1,5 +1,6 @@
 import os
 from flask import Flask, jsonify
+from flasgger import swag_from
 
 from flask_auth.app.common.logger import Logger
 
@@ -7,6 +8,7 @@ import flask_auth.app.extensions.restful.rest_framework as rest_framework
 import flask_auth.app.extensions.database.database_framework as database_framework
 import flask_auth.app.extensions.migrate.migrate_framework as migrate_framework
 import flask_auth.app.extensions.serializer.serializer_framework as serializer_framework
+import flask_auth.app.extensions.documentation.documentation_framework as documentation_framework
 
 import flask_auth.app.blueprints.users as user_module
 
@@ -25,12 +27,17 @@ def minimal_app(**config):
 
 def main_app(**config):
     # Extensions
-    app = minimal_app()
+    app: Flask = minimal_app()
 
     @app.route("/")
+    @swag_from("./res/swagger.yml")
     def index_route():
-        return jsonify(message="Server is running...")
+        return jsonify(
+            message="Server is running...",
+            documentation=f"http://localhost:{app.config.get('PORT')}/api/docs",
+        )
 
+    # Extensions (Plugins)
     database_framework.init_app(app)
     logger.info("Finish to initialize extension: Database")
     rest_framework.init_app(app)
@@ -39,8 +46,10 @@ def main_app(**config):
     logger.info("Finish to initialize extension: Migrate")
     serializer_framework.init_app(app)
     logger.info("Finish to initialize extension: Serializer")
+    documentation_framework.init_app(app)
+    logger.info("Finish to initialize extension: Documentation")
 
-    # Blueprints
+    # Blueprints (Modules)
     user_module.init_app(app)
     logger.info("Finish to initialize blueprint: Users")
 
