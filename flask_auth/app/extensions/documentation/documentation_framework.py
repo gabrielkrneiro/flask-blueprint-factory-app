d1666e0056027b610f771ef948ff057f5ab7b102
Gabriel Carneiro <carneiro.development@gmail.com>
2020-05-10 03:39:13 -0400

    docs: Add summary to routes

--- flask_auth/app/extensions/documentation/documentation_framework.py
+++ flask_auth/app/extensions/documentation/documentation_framework.py
@@ -8,7 +8,7 @@ def init_app(app: Flask):
     app.config["SWAGGER"] = {
         "title": "Flask Auth",
         "version": "1.0.0",
-        "description": "Open Source project to demonstrate Factory App + Blueprints pattern usage in Python projects",
+        "description": "Open Source project to demonstrate Factory App + Blueprints usage in Python projects",
         "license": "MIT",
         "termsOfService": "https://github.com/GabrielCarneiroDeveloper/flask-blueprint-factory-app",
     }
